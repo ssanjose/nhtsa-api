75fb769b93b0a44b627e65c29f5e2028e6eba943
ssanjose <ssanjose@academic.rrc.ca>
2022-02-11 23:22:27 -0600

    Adds comment in index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ dotenv.config();
 import { app } from './app';
 import http from 'http';
 
+// Configuration and setting up the server
 const port = process.env.NODE_ENV === ("development" || "test") ? 3000 : 8080;
 const server = http.createServer(app);
 
