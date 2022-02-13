77093c74bac5e7241dc837077392fc83b7275d77
ssanjose <ssanjose@academic.rrc.ca>
2022-02-12 23:39:36 -0600

    Places tests between different versions

--- test/v1/misc/misc.test.ts
+++ test/v1/misc/misc.test.ts
@@ -1,6 +1,6 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
-import { server } from '../../src/index';
+import { server } from '../../../src/index';
 
 const should = chai.should();
 chai.use(chaiHttp);
