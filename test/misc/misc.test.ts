5ececfc99b6c3ad7c6524822f74142fe67087283
ssanjose <ssanjose@academic.rrc.ca>
2022-02-12 23:03:58 -0600

    Added misc test

---
+++ test/misc/misc.test.ts
@@ -0,0 +1,19 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import { server } from '../../src/index';
+
+const should = chai.should();
+chai.use(chaiHttp);
+
+describe('Miscellaneous', () => {
+    describe('ping server /api/alive', () => {
+        it('should return a 200 response', (done) => {
+            chai.request(server)
+                .get('/api/alive')
+                .end((err: Error, res) => {
+                    res.should.have.status(200);
+                    done();
+                });
+        });
+    });
+});
