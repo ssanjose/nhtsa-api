adadbfc68379e64a65e7d0881ba0776def11bc25
ssanjose <ssanjose@academic.rrc.ca>
2022-02-13 00:09:44 -0600

    Adjusted test structure

--- test/v1/misc.test.ts
+++ test/v1/misc.test.ts
@@ -1,13 +1,13 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
-import { server } from '../../../src/index';
+import { server } from '../../src/index';
 
 const should = chai.should();
 chai.use(chaiHttp);
 
-describe('Miscellaneous2', () => {
-    describe('ping server /api/alive2', () => {
-        it('should return a 200 response2', (done) => {
+describe('Miscellaneous', () => {
+    describe('ping server /api/alive', () => {
+        it('should return a 200 response', (done) => {
             chai.request(server)
                 .get('/api/alive')
                 .end((err: Error, res) => {
