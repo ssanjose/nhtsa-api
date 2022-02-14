2c0a38b997b6fad7ac1b13e0c777032f0556dab5
ssanjose <ssanjose@academic.rrc.ca>
2022-02-13 22:41:25 -0600

    Add routes for model and adjust path to make

--- src/routes/api/v1/v1.routes.ts
+++ src/routes/api/v1/v1.routes.ts
@@ -1,10 +1,18 @@
 import express, { Request, Response } from "express"
 import { getMakesHandler } from "./controllers/make.controller"
+import { getModelsHandler } from "./controllers/model.controller";
 export const v1Routes = express.Router();
 
 // Set up routes.
-v1Routes.get('/getMakes', (req: Request, res: Response) => {
+
+// Get makes by calling getMakesHandler.
+v1Routes.get('/make', (req: Request, res: Response) => {
     getMakesHandler(req, res);
+});
+
+// Get models using make and year by calling getModelsForMakeHandler.
+v1Routes.get('/model', (req: Request, res: Response) => {
+    getModelsHandler(req, res);
 });
 
 v1Routes.get('/ping', (req: Request, res: Response) => {
