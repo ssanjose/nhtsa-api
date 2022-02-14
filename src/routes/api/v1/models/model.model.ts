bae7b38a903161e1d90e1aeec3345c2d516d762e
ssanjose <ssanjose@academic.rrc.ca>
2022-02-13 23:20:14 -0600

    Separates model interfaces to response from vin, normal api, and model to be return

--- src/routes/api/v1/models/model.model.ts
+++ src/routes/api/v1/models/model.model.ts
@@ -1,7 +1,25 @@
-export interface Model {
+// Response from the model API.
+export interface ModelResponse {
     Make_ID?: number;
     Model_ID?: number;
     Make_Name: string;
     Model_Name: string;
     Model_Year: number;
+}
+
+// Response from the VIN API.
+export interface ModelVINResponse {
+    VIN: string;
+    Make: string;
+    Model: string;
+    ModelYear: number;
+}
+
+// Model object.
+export interface Model {
+    name: string;
+    make?: string;
+    model?: string;
+    modelYear?: number;
+    vin?: string;
 }
