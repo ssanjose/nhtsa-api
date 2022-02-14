ef97ba36611e108ee817270dc01d09d4a2938e7c
ssanjose <ssanjose@academic.rrc.ca>
2022-02-13 23:22:20 -0600

    Adjusts getModelsHandler to check query parameters to choose between different functions

--- src/routes/api/v1/controllers/model.controller.ts
+++ src/routes/api/v1/controllers/model.controller.ts
@@ -1,9 +1,18 @@
 import { Request, Response } from "express";
-import { getModelsForMakeYear } from "../services/model.service";
+import { getModelForVin, getModelsForMakeYear } from "../services/model.service";
 
 export const getModelsHandler = async (req: Request, res: Response) => {
     try {
-        return await getModelsForMakeYear(req, res);
+        // Get make and year from req query string.
+        const make = req.query.make;
+        const year = req.query.year;
+        const vin = req.query.vin;
+
+        // Check if vin is provided. If it is, get the model for the vin using getModelForVin.
+        if (vin)
+            return await getModelForVin(req, res, vin);
+        else if (!!make && !!year)
+            return await getModelsForMakeYear(req, res, make, year);
     } catch (error: any) {
         res.status(500).json({
             error: {
