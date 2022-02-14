122a0eed284e02ef1b3f4ae4a3665aec8a11e35a
ssanjose <ssanjose@academic.rrc.ca>
2022-02-13 23:21:32 -0600

    Adds getModelForVin function and adjusts model typing

--- src/routes/api/v1/services/model.service.ts
+++ src/routes/api/v1/services/model.service.ts
@@ -1,13 +1,10 @@
 import axios from "axios";
 import { Request, Response } from "express";
-import { Model } from "../models/model.model";
+import { Model, ModelResponse, ModelVINResponse } from "../models/model.model";
 
-export const getModelsForMakeYear = async (req: Request, res: Response) => {
+// Get the models using make and year.
+export const getModelsForMakeYear = async (req: Request, res: Response, make: any, year: any) => {
     try {
-        // Get make and year from req query string.
-        const make = req.query.make;
-        const year = req.query.year;
-
         // Check if make and year are provided. If not return error.
         if (!make || !year)
             return res.status(400).json({
@@ -19,10 +16,35 @@ export const getModelsForMakeYear = async (req: Request, res: Response) => {
         const response = await axios
             .get(`https://vpic.nhtsa.dot.gov/api/vehicles/getmodelsformakeyear/make/${make}/modelyear/${year}?format=json`);
 
-        const models: Model[] = response.data.Results.map((model: Model) => {
-            return `${model.Make_Name} ${model.Model_Name}`
+        const models: Model[] = response.data.Results.map((model: ModelResponse) => {
+            return { name: `${model.Make_Name} ${model.Model_Name}` }
         });
-        res.json({ model_names: models });
+        res.json({ models });
+    }
+    catch (error: any) {
+        res.status(500).json({
+            error: {
+                message: error.message,
+            },
+        });
+    }
+}
+
+// Get the model using vin.
+export const getModelForVin = async (req: Request, res: Response, vin: any) => {
+    try {
+        const response = await axios
+            .get(`https://vpic.nhtsa.dot.gov/api/vehicles/decodevinvalues/${vin}?format=json`);
+
+        const modelFromApi: ModelVINResponse = response.data.Results[0];
+        const model: Model = {
+            name: `${modelFromApi.ModelYear} ${modelFromApi.Make} ${modelFromApi.Model}`,
+            make: modelFromApi.Make,
+            model: modelFromApi.Model,
+            modelYear: modelFromApi.ModelYear,
+            vin: modelFromApi.VIN
+        };
+        res.json({ model: model });
     }
     catch (error: any) {
         res.status(500).json({
