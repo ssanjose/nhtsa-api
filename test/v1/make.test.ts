d1a8f3865b3c1e6c034cd8f409f432a1e4ae2d5b
ssanjose <ssanjose@academic.rrc.ca>
2022-02-13 22:37:22 -0600

    Change api path on make test.ts

--- test/v1/make.test.ts
+++ test/v1/make.test.ts
@@ -9,7 +9,7 @@ describe('Make Test', () => {
     describe('Get all makes', () => {
         it('should return a 200 response and list of makes', (done) => {
             chai.request(server)
-                .get('/api/v1/getMakes')
+                .get('/api/v1/make')
                 .end((err: Error, res) => {
                     res.should.have.status(200);
                     res.body.should.be.a('object');
