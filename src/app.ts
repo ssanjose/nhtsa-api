293a4fbcd09d5494b75020d9ef28e18cfc131115
ssanjose <ssanjose@academic.rrc.ca>
2022-06-21 14:42:36 -0500

    Add cors to server

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { NextFunction, Request, Response } from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
 import helmet from 'helmet';
 import { routes } from './routes/routes';
 
@@ -8,6 +9,11 @@ export const app = express();
 //  ----------------------------------------------------------------
 //  ----------------    MIDDLEWARE
 if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
+app.use(cors({
+  origin: ["https://vpic.nhtsa.dot.gov", "http://localhost:3000", "http://localhost:3001", "*"],
+  methods: ["GET"],
+  allowedHeaders: ["Content-Type", "Origin", "X-Requested-With", "Accept"],
+}));
 app.use(helmet());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -19,12 +25,12 @@ app.use('/api', routes);
 //  ----------------------------------------------------------------
 //  ----------------   ERROR HANDLING REQUESTS
 app.use((req: Request, res: Response, next: NextFunction) => {
-    const error = new Error("Not found");
-    next(error);
+  const error = new Error("Not found");
+  next(error);
 }, (error: Error, req: Request, res: Response, next: NextFunction) => {
-    res.status(404).json({
-        error: {
-            message: error.message,
-        },
-    });
+  res.status(404).json({
+    error: {
+      message: error.message,
+    },
+  });
 });
--- test/v2/misc.test.ts
+++ test/v2/misc.test.ts
@@ -5,9 +5,9 @@ import { server } from '../../src/index';
 const should = chai.should();
 chai.use(chaiHttp);
 
-describe('Miscellaneous2', () => {
-    describe('ping server /api/alive2', () => {
-        it('should return a 200 response2', (done) => {
+describe('Miscellaneous v2', () => {
+    describe('ping server /api/v1/ping2', () => {
+        it('should return a 200 response v2', (done) => {
             chai.request(server)
                 .get('/api/alive')
                 .end((err: Error, res) => {
